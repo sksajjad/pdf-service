ec0ad1e041eb7a8f0eeb4261fdffc0a84b1e11c5
Sajjad Hossain <109612279+sksajjad@users.noreply.github.com>
2026-08-03 00:34:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/pdf", async (req, res) => {
 
     const page = await browser.newPage();
 
-    await page.goto(req.body.html, {
+    await page.setContent(req.body.html, {
         waitUntil: "networkidle0"
     });
 
