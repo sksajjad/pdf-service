a6c34f5c574ffbd48f1c54fc405fe33003c13092
Sajjad Hossain <109612279+sksajjad@users.noreply.github.com>
2026-08-03 00:36:41 +0600

    Remove executablePath for Puppeteer launch
    Remove explicit path to Chromium executable.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.post("/pdf", async (req, res) => {
 
         const browser = await puppeteer.launch({
             headless: true,
-            executablePath: "/usr/bin/chromium",
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox"
