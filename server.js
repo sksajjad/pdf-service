7efeb6d06f11e796ac59004cacbe0df1006c67aa
Sajjad Hossain <109612279+sksajjad@users.noreply.github.com>
2026-08-03 00:28:09 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,8 @@ const puppeteer = require("puppeteer");
 const app = express();
 
 app.use(express.json({ limit: "20mb" }));
-app.get("/", (req, res) => {
-    res.send("PDF Server is Running");
-});
 
-app.get("/test", (req, res) => {
-    res.send("OK");
-});
 app.post("/pdf", async (req, res) => {
-    res.send("POST /pdf reached");
     const browser = await puppeteer.launch({
         headless: true,
         executablePath: "/usr/bin/chromium",
